d1d542d12b2e3ee81d9abb076b17883e6ff938c6
starhaotian <starhaotian@gmail.com>
2017-11-23 12:51:27 -0800

    data

--- src/dataset.java
+++ src/dataset.java
@@ -13,7 +13,7 @@ public class dataset {
         String[] intensive_id = {"0001","0002","0003","0004","0005"};
         String[] title = {"Halloween", "Thanksgiving", "Customer Service Day", "Christmas","Independence Day"};
         float[] discount = {500, 300, 1500, 800, 600};
-        String[] criterion = {"ALL", "Toyota,2016,Manual transmission","Camry，2010，white","Volkswagen,red","2013"};
+        String[] criterion = {"ALL", "no,white,toyoto,no,no","no,no,no,no,2017","no,black,no,SUV,no","no,no,Volkswagen,Saloon,2015"};
         String[] start_date = {"2012.2.1", "2014.2.5","2016.5.8","2015.11.23","2016.7.4"};
         String[] end_date = {"2017.1.2", "2017.5.6","2017.2.5","2017.10.2","2017.11.24"};
         String[] description = {"this is a huge discount", "buy now", "do not miss it", "you could get a extra gas gift card", "you could get an addition"};
