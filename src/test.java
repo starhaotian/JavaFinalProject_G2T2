eb6f0d7a48eb1847d0d0236d26a18a2688cd1f68
starhaotian <starhaotian@gmail.com>
2017-11-26 20:55:33 -0800

    // 与Group2 repo同步Incentive 增加datasample

--- src/test.java
+++ src/test.java
@@ -6,11 +6,11 @@ import java.util.List;
  * Created by starhaotian on 22/11/2017.
  */
 public class test {
-    public static void main(String[] args){
-        String s= "201,12,31,21";
+    public static void main(String[] args) {
+        String s = "201,12,31,21";
         String[] ar = s.split(",");
-        List l=new ArrayList();
-        l=Arrays.asList(ar);
+        List l = new ArrayList();
+        l = Arrays.asList(ar);
         System.out.println(l);
         l.add("222");
     }
