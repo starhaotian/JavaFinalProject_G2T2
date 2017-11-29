84e9e0232642fd9328cc06a6234c3022f4b8157a
NorthernAlchemist <31714103+NorthernAlchemist@users.noreply.github.com>
2017-11-29 11:31:02 -0800

    LQ

--- src/IncentiveDeleteDialog.java
+++ src/IncentiveDeleteDialog.java
@@ -7,21 +7,24 @@ public class IncentiveDeleteDialog extends JDialog {
     private JButton submitButton;
 
     public IncentiveDeleteDialog(){
-        setSize(500,800);
+        setTitle("delete incentive");
+        setSize(300,300);
+        setVisible(true);
 
         explaination = new JLabel("Please put the incentive ID you want to delete");
         textField = new JTextField();
-        textField.setSize(100,80);
+        textField.setPreferredSize(new Dimension(100, 35));
         submitButton = new JButton("Submit");
+        display();
     }
 
     public void display(){
         setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
         gc.gridy = 1;
-        gc.gridx = 1;
+        gc.gridx = 0;
         gc.weightx = 1;
-        gc.weighty = 1;
+        gc.weighty = 0.1;
         gc.fill = GridBagConstraints.NONE;
         gc.insets = new Insets(5,5,5,5);
 
@@ -31,9 +34,10 @@ public class IncentiveDeleteDialog extends JDialog {
 
 
         gc.gridy++;
-        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.anchor = GridBagConstraints.CENTER;
         add(textField, gc);
 
+        gc.weighty = 1;
         gc.gridy++;
         gc.anchor = GridBagConstraints.CENTER;
         add(submitButton, gc);
